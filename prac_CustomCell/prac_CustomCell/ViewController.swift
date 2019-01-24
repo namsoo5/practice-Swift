908c4533bd04eedb64b455bc9cfddcf99f3a0024
namsoo <are34@naver.com>
2019-01-24 16:06:02 +0900

    Custom Table View 디자인구성

---
+++ prac_CustomCell/prac_CustomCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prac_CustomCell
+//
+//  Created by 남수김 on 24/01/2019.
+//  Copyright © 2019 남수김. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
