75632532e1b5c609a96a73a9fbbe05059120f13d
namsoo5 <are34@naver.com>
2019-12-09 14:55:57 +0900

    CircleTransition 효과 구현

---
+++ prac_Animation/prac_Animation/Sources/VC/Circle/CircleOneVC.swift
@@ -0,0 +1,30 @@
+//
+//  CircleOneVC.swift
+//  prac_Animation
+//
+//  Created by 남수김 on 2019/12/08.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class CircleOneVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ prac_Animation/prac_Animation/Sources/VC/Circle/CircularTransition.swift
@@ -0,0 +1,13 @@
+//
+//  CircularTransition.swift
+//  prac_Animation
+//
+//  Created by 남수김 on 2019/12/08.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class CircularTransition: NSObject {
+
+}
