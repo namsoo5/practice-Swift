34fad7527cc9c71a41ffdd3c4ac1163d48f7cb63
namsoo5 <are34@naver.com>
2019-12-13 00:01:55 +0900

    SocketIO 설치

---
+++ prac_Socket/Pods/Target Support Files/Pods-prac_Socket/Pods-prac_Socket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_prac_Socket : NSObject
+@end
+@implementation PodsDummy_Pods_prac_Socket
+@end
---
+++ prac_Socket/prac_Socket/Sources/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  prac_Socket
+//
+//  Created by 남수김 on 2019/11/19.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func connectSocket(_ sender: Any) {
+       
+        
+    }
+    
+    @IBAction func sendData(_ sender: Any) {
+     
+    }
+    
+    
+}
+
