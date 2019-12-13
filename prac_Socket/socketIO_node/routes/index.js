0abbb4963c17cbc1c8624c6ab51c444f6e941547
namsoo5 <are34@naver.com>
2019-12-13 16:01:05 +0900

    SocketIO서버 - 룸구현, 메시지 emit, on 구현

--- prac_Socket/socketIO_node/routes/index.js
+++ prac_Socket/socketIO_node/routes/index.js
@@ -1,36 +1,133 @@
+
+
+var app = require('express');
+var router = app.Router();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const room = io.of('/test');
+
+router.get('/', function (req, res) {
+  room.emit('test', { 
+    'Hello' : 'World',
+    'test' : 'good'
+ });
+  res.send('<h1>AppCoda - SocketChat Server</h1>');
+});
+
+
+http.listen(9000, function () {
+  console.log('Listening on *:9000');
+  
+});
+
+room.on('connection', function (clientSocket) {
+  console.log('a user connected');
+  clientSocket.join("/test")
+
+  clientSocket.on('event', (msg) => {
+    console.log("some..")
+    console.log(msg)
+    console.log(msg["message"])
+  })
+
+  clientSocket.on('disconnect', function () {
+    console.log('user disconnected');
+  })
+})
+
+
+
+/*
 var express = require('express');
 var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+const socketIO = require('socket.io');
 
-module.exports = router;
 
+module.exports = (server,app)=>{
+  const io = socketIO(server);
+
+  server.listen(9000, ()=>{
+    console.log("listen,,,")
+  })
+  // const room = io.of('/room');
+  // const chat = io.of('/chat');
+  const room = io.of('/test');
+
+  app.set('io',io);
+
+  io.on('connection', (socket) => {
+      const clientId = socket.id;
+      socket.on('disconnect',()=>{})
+  });
+
+  room.on('connection',(socket)=>{
+      let  roomListId;
+      console.log("connect!!!")
+      socket.on('init',(initdata)=>{
+              roomListId = initdata._id;
+              socket.join(roomListId)
+      });
+      socket.on('disconnect',()=>{
+        console.log("disconnet!!")
+          socket.leave(roomListId)
+      });
+
+  });
+
+  chat.on('connection',(socket)=>{
+      let currentRoomId;
+
+      socket.on('init',(initdata)=>{
+          currentRoomId = initdata.roomId;;
+          socket.join(currentRoomId)
+      });
+
+      // socket.on('newChat',(data)=>{
+      //     axios.post('http://localhost:9999/chat/newChat',data);
+      // });
+
+      socket.on('call',()=>{
+          socket.to(chat).emit('call');
+      });
+
+      socket.on('disconnect',()=>{
+          console.log(`채팅 네임스페이스 해제 ${currentRoomId}`);
+          socket.leave(currentRoomId);
+      })
+
+  });
+
+
+};
+*/
+/*
 var app = require('express')();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 // http server를 socket.io server로 upgrade한다
-var io = require('socket.io')(server);
+var io = require('socket.io')(server)
+
+console.log("testing...")
+
+server.listen(9000, (data) => {
+  console.log('listen!!')
+})
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  io.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('event1', (msg) => {
+    console.log(msg);
+    });
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
   });
-  io.on('disconnect', () => {
-  console.log('user disconnected');
-  });
-});
-io.listen(3001, () => {
-  console.log('Connected at 3001');
 });
 
+
+*/
 /*
-socket.on('event_name', function (data) {
-  console.log('Message from Client: ' + data);
-});
-
 // 접속된 모든 클라이언트에게 메시지를 전송한다
 io.emit('event1', msg);
 
@@ -63,12 +160,13 @@ socket.on('chat', function (data) {
 //     socket.disconnect();
 //   })
 
-//   socket.on('disconnect', function() {
-//     console.log('user disconnected: ' + socket.name);
-//   });
+  // socket.on('disconnect', function() {
+  //   console.log('user disconnected: ' + socket.name);
+  // });
 // });
-
 // server.listen(3000, function() {
 //   console.log('Socket IO server listening on port 3000');
 // });
 */
+
+module.exports = router;
