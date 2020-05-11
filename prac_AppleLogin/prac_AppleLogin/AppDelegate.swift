c69b86d0fb00cdfb128eb0d9d1ef3b6ddc6da68a
namsoo5 <are34@naver.com>
2020-05-11 23:52:33 +0900

    apple login 연습

---
+++ prac_AppleLogin/prac_AppleLogin/AppDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.swift
+//  prac_AppleLogin
+//
+//  Created by 남수김 on 2020/04/14.
+//  Copyright © 2020 ns. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: "001720.b9123c16bbf74c0d996a76a8f29c6585.0830") { (credentialState, error) in
+            switch credentialState {
+            case .authorized:
+                print("authorized")
+            // The Apple ID credential is valid.
+            case .revoked:
+                print("revoked")
+            case .notFound:
+                // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+                print("notFound")
+                DispatchQueue.main.async {
+                    // self.window?.rootViewController?.showLoginViewController()
+                }
+            default:
+                break
+            }
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ prac_AppleLogin/prac_AppleLogin/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  prac_AppleLogin
+//
+//  Created by 남수김 on 2020/04/14.
+//  Copyright © 2020 ns. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var testView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupProviderLoginView()
+       
+    }
+    
+    func setupProviderLoginView() {
+        let appleButton = ASAuthorizationAppleIDButton(type: .default, style: .whiteOutline)
+        appleButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.testView.addSubview(appleButton)
+        
+        appleButton.translatesAutoresizingMaskIntoConstraints = false
+        appleButton.leadingAnchor.constraint(equalTo: testView.leadingAnchor).isActive = true
+        appleButton.trailingAnchor.constraint(equalTo: testView.trailingAnchor).isActive = true
+        appleButton.topAnchor.constraint(equalTo: testView.topAnchor).isActive = true
+        appleButton.bottomAnchor.constraint(equalTo: testView.bottomAnchor).isActive = true
+    }
+    
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+extension ViewController:
+    ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // Create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            let token = appleIDCredential.identityToken
+            let authCode = appleIDCredential.authorizationCode
+            
+            print("useridentifier: \(userIdentifier)")
+            print("fullName: \(fullName)")
+            print("email: \(email)")
+            print("token: \(token)")
+            print("authCode: \(authCode)")
+            
+        case let passwordCredential as ASPasswordCredential:
+            // Sign in using an existing iCloud Keychain credential.
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            print("username: \(username)")
+            print("password: \(password)")
+            
+        default:
+            break
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("--login err")
+    }
+}
