698df51f82398777eeb0eb4944b4873ba8d6da7d
namsoo5 <are34@naver.com>
2020-11-08 17:11:34 +0900

    UPDATE: 앱라이프사이클 연습

---
+++ prac_SceneLifeCycle/prac_SceneLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prac_SceneLifeCycle
+//
+//  Created by 남수김 on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
